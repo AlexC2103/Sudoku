50070fea2f756660843ba282233ae93576d6da9d
AlexC2103 <80043678+AlexC2103@users.noreply.github.com>
2021-07-13 14:44:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function createBoard() {
     boardStart.appendChild(br);
   }
 
-  for (var i = 1; i <= 9; ++i) {
+  for (var i = 1; i <= 10; ++i) {
     boardStart = document.getElementById('keyboard');
     var keyboardButton = document.createElement('BUTTON');
     var id = 100 + i;
@@ -30,9 +30,16 @@ function createBoard() {
     boardStart.appendChild(keyboardButton);
     keyboardButton.setAttribute('id', id);
     keyboardButton.setAttribute('class', 'keyboardButton');
-    keyboardButton.setAttribute('onclick', 'placeNumber(this.id)');
+    if (i <= 9) {
+      keyboardButton.setAttribute('onclick', 'placeNumber(this.id)');
 
-    document.getElementById(id).innerHTML = i;
+      document.getElementById(id).innerHTML = i;
+    }
+
+    if (i === 10) {
+      keyboardButton.setAttribute('onclick', 'eraseNumber(this.id)');
+      document.getElementById(id).innerHTML = '<i class="fas fa-eraser"></i>';
+    }
   }
 
   for (var i = 2; i < 81; i += 3) {
@@ -145,4 +152,107 @@ function placeNumber(keyboardButtonId) {
     selectedCell[0].innerHTML = keyboardButtonId % 10;
     gameMatrix[line][column] = parseInt(selectedCell[0].innerHTML);
   }
+
+  notInLine(gameMatrix, line, column);
+  notInColumn(gameMatrix, line, column);
+  notInBox(gameMatrix, line, column, line - line % 3, column - column % 3);
+}
+
+function eraseNumber(keyboardButtonId) {
+  var selectedCell = document.getElementsByClassName('selectedCell');
+
+  var line = parseInt(selectedCell[0].id / 9);
+  var column = selectedCell[0].id - line * 9;
+
+  if (selectedCell.length !== 0) {
+    selectedCell[0].innerHTML = '';
+    gameMatrix[line][column] = 0;
+  }
+
+  notInLine(gameMatrix, line, column);
+  notInColumn(gameMatrix, line, column);
+  notInBox(gameMatrix, line, column, line - line % 3, column - column % 3);
+}
+
+function notInLine(gameMatrix, line, column) {
+  var st = new Set();
+  if (gameMatrix[line][column] !== 0) {
+    st.add(gameMatrix[line][column]);
+  }
+
+  for (var i = 0; i < 9; ++i) {
+    if (i !== column) {
+      if (st.has(gameMatrix[line][i])) {
+        document.getElementById(line * 9 + i).style.color = 'red';
+        return false;
+      }
+    }
+
+    if (gameMatrix[line][i] !== 0) {
+      st.add(gameMatrix[line][i]);
+    }
+  }
+
+  for (var i = 0; i < 9; ++i) {
+    document.getElementById(line * 9 + i).style.color = 'black';
+  }
+
+  return true;
+}
+
+function notInColumn(gameMatrix, line, column) {
+  var st = new Set();
+  if (gameMatrix[line][column] !== 0) {
+    st.add(gameMatrix[line][column]);
+  }
+
+  for (var i = 0; i < 9; ++i) {
+    if (i !== line) {
+      if (st.has(gameMatrix[i][column])) {
+        document.getElementById(i * 9 + column).style.color = 'red';
+        return false;
+      }
+    }
+
+    if (gameMatrix[i][column] !== 0) {
+      st.add(gameMatrix[i][column]);
+    }
+  }
+
+  for (var i = 0; i < 9; ++i) {
+    document.getElementById(i * 9 + column).style.color = 'black';
+  }
+
+  return true;
+}
+
+function notInBox(gameMatrix, line, column, startLine, startColumn) {
+  var st = new Set();
+  if (gameMatrix[line][column] !== 0) {
+    st.add(gameMatrix[line][column]);
+  }
+
+  for (var i = 0; i < 3; ++i) {
+    for (var j = 0; j < 3; ++j) {
+      var currentElement = gameMatrix[i + startLine][j + startColumn];
+      if (i !== line || j !== column) {
+        if (st.has(currentElement)) {
+          document.getElementById((i + startLine) * 9 + (j + startColumn)).style.color = 'red';
+          return false;
+        }
+      }
+
+      if (currentElement !== 0) {
+        st.add(currentElement);
+      }
+    }
+  }
+
+  for (var line = 0; line < 3; ++line) {
+    for (var column = 0; column < 3; ++column) {
+      document.getElementById((line + startLine) * 9 + (column + startColumn)).style.color = 'black';
+    }
+  }
+
+  return true;
 }
